b0b9a9aa4ac3e439bd5d677c659bc5333dcc2b11
liuxianghua1 <3374312139.@qq.com>
2022-10-01 21:33:29 +0800

    后台界面初始化

---
+++ admin/src/views/Main.vue
@@ -0,0 +1,44 @@
+<template>
+  <el-container style="height: 100vh; border: 1px solid #eee">
+    <el-header style="text-align: right; font-size: 12px">
+      <span>王小虎</span>
+    </el-header>
+
+    <el-container>
+      <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
+        <el-menu router unique-opened :default-active="$route.path">
+          <el-menu-item index="/home"><i class="el-icon-house"></i>主页</el-menu-item>
+          <el-submenu index="1">
+            <template slot="title"><i class="el-icon-user"></i>用户管理</template>
+            <el-menu-item-group>
+              <el-menu-item index="/home/user_list">用户列表</el-menu-item>
+            </el-menu-item-group>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <el-main>
+        <router-view :key="$route.path"></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<style>
+.el-header {
+  background-color: #b3c0d1;
+  color: #333;
+  line-height: 60px;
+}
+
+.el-aside {
+  color: #333;
+}
+</style>
+
+<script>
+export default {
+  data() {
+    return {}
+  }
+}
+</script>
